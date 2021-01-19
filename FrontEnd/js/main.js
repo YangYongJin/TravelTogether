fdf4b90248b8ca2a8810cf7ac23149d5273684f2
YangYongJin <dyyjkd@gmail.com>
2021-01-20 00:46:38 +0900

    add scroll fade on main page

--- FrontEnd/js/main.js
+++ FrontEnd/js/main.js
@@ -1,28 +1,56 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 // Next/previous controls
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+  showSlides((slideIndex += n));
 }
 
 // Thumbnail image controls
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+  showSlides((slideIndex = n));
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
 }
+
+let fadeElements = document.querySelectorAll(".hide");
+window.addEventListener("scroll", function (e) {
+  fadeElements = Array.from(fadeElements);
+  fadeElements.forEach(function (item) {
+    let posFromTop = item.getBoundingClientRect().top;
+    // let scrollY = window.scrollY;
+    let winH = window.innerHeight;
+
+    /* 3 */
+    if (winH > posFromTop) {
+      let timeDuration = 3000;
+      item.animate(
+        { opacity: "1" },
+        {
+          duration: timeDuration,
+        }
+      );
+      setTimeout(() => {
+        item.style.opacity = 1;
+      }, timeDuration);
+    }
+  });
+});
